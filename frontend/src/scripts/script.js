ecce0ed40a1f1f7743f395c47b0a41eccfc230a1
Aya Hafri <115592428+hafriaya@users.noreply.github.com>
2024-04-01 11:32:06 +0000

    load_page_on_resize

--- frontend/src/scripts/script.js
+++ frontend/src/scripts/script.js
@@ -1,60 +1,71 @@
 var timeOut = 2000;
 var slideIndex = 0;
 var autoOn = true;
-if (window.innerWidth < 700) {
-  autoSlides();
+(function () {
+  var width = window.innerWidth;
 
-  function autoSlides() {
-    timeOut = timeOut - 20;
-
-    if (autoOn == true && timeOut < 0) {
-      showSlides();
+  window.addEventListener("resize", function () {
+    if (window.innerWidth !== width) {
+      width = window.innerWidth;
+      if (window.innerWidth < 700) {
+        autoSlides();
+        window.location.reload(true);
+      }
     }
-    setTimeout(autoSlides, 20);
+  });
+})();
+
+function autoSlides() {
+  timeOut = timeOut - 20;
+
+  if (autoOn == true && timeOut < 0) {
+    showSlides();
   }
+  setTimeout(autoSlides, 20);
+  console.log("autoSlides is working");
+}
 
-  function prevSlide() {
-    timeOut = 2000;
+function prevSlide() {
+  timeOut = 2000;
 
-    var slides = document.getElementsByClassName(
-      "collection__products_container"
-    );
-    var dots = document.getElementsByClassName("dot");
+  var slides = document.getElementsByClassName(
+    "collection__products_container"
+  );
+  var dots = document.getElementsByClassName("dot");
 
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slideIndex--;
-
-    if (slideIndex > slides.length) {
-      slideIndex = 1;
-    }
-    if (slideIndex == 0) {
-      slideIndex = 3;
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    dots[i].className = dots[i].className.replace(" active", "");
   }
+  slideIndex--;
 
-  function showSlides() {
-    timeOut = 2000;
-
-    var slides = document.getElementsByClassName(
-      "collection__products_container"
-    );
-    var dots = document.getElementsByClassName("dot");
-
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slideIndex++;
-
-    if (slideIndex > slides.length) {
-      slideIndex = 1;
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
   }
+  if (slideIndex == 0) {
+    slideIndex = 3;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+function showSlides() {
+  timeOut = 2000;
+
+  var slides = document.getElementsByClassName(
+    "collection__products_container"
+  );
+  var dots = document.getElementsByClassName("dot");
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slideIndex++;
+
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
